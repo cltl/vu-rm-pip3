cdb7d1b858190c526a0464a4ea4b11376dd5c08d
sarnoult <sophie.arnoult@posteo.net>
2019-04-11 15:33:43 +0200

    configuring read-the-docs build

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,12 @@ release = '3.0'
 
 # -- General configuration ---------------------------------------------------
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -54,3 +60,4 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
