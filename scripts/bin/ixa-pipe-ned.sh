210791168c9ee1b21e8b90dd19845f1140a22ac3
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-11-09 13:36:50 +0100

    Make sure ixa-pipe-ned can also work with docker

--- scripts/bin/ixa-pipe-ned.sh
+++ scripts/bin/ixa-pipe-ned.sh
@@ -3,12 +3,12 @@
 # component: ixa-pipe-ned
 #----------------------------------------------------
 
- 
+PORT=2020
 workdir=$(cd $(dirname "${BASH_SOURCE[0]}") && cd ../.. && pwd)
 source $workdir/.newsreader
 modulesdir=$VURM_LIB/java
 resourcesdir=$VURM_LIB/resources
 scriptsdir=${workdir}/scripts
 cd $workdir
-$scriptsdir/util/connect-dbpedia-spotlight.sh $resourcesdir/spotlight
-java -jar $modulesdir/ixa-pipe-ned-1.1.6.jar -p 2060 -i $resourcesdir/spotlight/wikipedia-db -n nlEn
+$scriptsdir/util/connect-dbpedia-spotlight.sh $resourcesdir/spotlight $PORT
+java -jar $modulesdir/ixa-pipe-ned-1.1.6.jar -p $PORT -i $resourcesdir/spotlight/wikipedia-db -n nlEn
--- scripts/util/connect-dbpedia-spotlight.sh
+++ scripts/util/connect-dbpedia-spotlight.sh
@@ -4,19 +4,20 @@
 # is open, and launches spotlight server otherwise.
 #-----------------------------------------------------
 spotlightdir=$1
+port=$2
 echo $spotlightdir
-if ! lsof -Pi :2060 >/dev/null ; then
+if ! curl -I http://localhost:$port/rest/ > /dev/null 2>&1; then
   >&2 echo "launching dbpedia spotlight server"
-  java -jar -Xmx2000m $spotlightdir/dbpedia-spotlight-0.7.1.jar $spotlightdir/nl http://localhost:2060/rest &> spotlight.log &
+  java -jar -Xmx2000m $spotlightdir/dbpedia-spotlight-0.7.1.jar $spotlightdir/nl http://localhost:$port/rest &> spotlight.log &
 
   max=6
   i=0
-  while ! lsof -Pi :2060 >/dev/null && [ $i -le $max ]; do
+  while ! lsof -Pi :$port >/dev/null && [ $i -le $max ]; do
     >&2 echo "waiting for connection..."
     sleep 20
     i=$((i+1))
   done
-  if ! lsof -Pi :2060 >/dev/null; then
+  if ! lsof -Pi :$port >/dev/null; then
     >&2 echo "failed to establish connection in allocated time"
   fi
 else
